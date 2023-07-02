31a6e0e20a74f7580c07fc748d3541d62d672edf
hoda salah <hodasalah35@gmail.com>
2023-07-03 01:00:07 +0300

    finish navbar ui designing

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,13 +1,19 @@
 'use client';
+import {useCallback, useState} from 'react';
 import {AiOutlineMenu} from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleOpen = useCallback(() => {
+		setIsOpen((value) => !value);
+	}, []);
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
 				<div
-					onClick={() => {}}
+					onClick={toggleOpen}
 					className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
 				>
 					Airbnb your home
@@ -19,6 +25,16 @@ const UserMenu = () => {
 					<Avatar />
 				</div>
 			</div>
+			{isOpen && (
+				<div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+					<div className='flex flex-col cursor-pointer'>
+						<>
+							<MenuItem onClick={() => {}} label='Login' />
+							<MenuItem onClick={() => {}} label='Signup' />
+						</>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
