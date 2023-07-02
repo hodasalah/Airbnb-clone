52b8a4fdef4fd59dc000b0d6aea1f007493dcdc3
hoda salah <hodasalah35@gmail.com>
2023-07-03 00:28:46 +0300

    add navbar userMenu

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+'use client';
+import Image from 'next/image';
+import {useRouter} from 'next/navigation';
+const Logo = () => {
+	const router = useRouter();
+	return (
+		<Image
+			alt='airbnb logo'
+			width={100}
+			height={100}
+			src='/images/logo-2.png'
+			className='hidden md:block cursor-pointer'
+		/>
+	);
+};
+
+export default Logo;
