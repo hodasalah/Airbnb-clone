e86feb283802265a923bc20eb7cc9859f34076c3
hoda salah <hodasalah35@gmail.com>
2023-07-17 00:45:27 +0300

    readd middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,5 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+export { default } from "next-auth/middleware";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
-}
-
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     "/trips",
@@ -14,4 +7,4 @@ export const config = {
     "/properties",
     "/favorites"
   ]
-}
+};
